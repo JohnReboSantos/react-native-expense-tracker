2f284a8562593611d575ceac96c4cc3d9edf912b
rebo <rebo@mahusai.ph>
2023-07-05 01:06:03 +0800

    create loading component; put the components in app under StoreProvider parent

--- App.tsx
+++ App.tsx
@@ -3,16 +3,27 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import useInit from './hooks/useInit';
+import Loading from './components/Loading';
+import { StoreProvider } from './stores';
 import ExpenseForm from './components/ExpenseForm';
 
 export default function App() {
+  const {ready, store} = useInit();
   const Drawer = createDrawerNavigator();
+
+  if (!ready || !store) {
+    return <Loading />;
+  }
+
   return (
+    <StoreProvider value={store}>
     <NavigationContainer>
       <Drawer.Navigator>
       <Drawer.Screen name='Add an Expense' component={ExpenseForm} />
       </Drawer.Navigator>
     </NavigationContainer>
+    </StoreProvider>
   );
 }
 
