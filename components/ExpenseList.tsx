afde147d1e0dae35ea770eee217b7e848d79d86c
rebo <rebo@mahusai.ph>
2023-07-04 23:38:52 +0800

    make form to add an expense compatible with IOS

--- components/ExpenseList.tsx
+++ components/ExpenseList.tsx
@@ -1,4 +1,4 @@
-import { Platform, StyleSheet, Text, TextInput, SafeAreaView, Pressable, GestureResponderEvent } from "react-native";
+import { Platform, StyleSheet, Text, TextInput, SafeAreaView, Pressable, GestureResponderEvent, View } from "react-native";
 import DateTimePicker, { DateTimePickerEvent } from "@react-native-community/datetimepicker";
 import React, { useState } from "react";
 import { observer } from 'mobx-react-lite'
@@ -27,6 +27,11 @@ export const ExpenseList = () => {
         }
     }
 
+    const confirmIOSDate = () => {
+        setFormData({...formData, date: selectedDate.toDateString()});
+        toggleDatepicker();
+    };
+
     const onSubmit = (e: GestureResponderEvent) => {
        e.preventDefault();
        setFormData({...formData, date: selectedDate.toString()});
@@ -39,9 +44,26 @@ export const ExpenseList = () => {
            <TextInput style={styles.input} placeholder='Name' value={formData.name} onChangeText={e => setFormData({...formData, name: e})}/>
            <TextInput style={styles.input} placeholder='Description' multiline numberOfLines={4} value={formData.description} onChangeText={e => setFormData({...formData, description: e})}/>
            <TextInput style={styles.input} placeholder='Category' value={formData.category} onChangeText={e => setFormData({...formData, category: e})}/>
-           {showPicker && (<DateTimePicker mode='date' display='spinner' value={selectedDate} onChange={onChange} />)}
+           {showPicker && (<DateTimePicker mode='date' display='spinner' value={selectedDate} onChange={onChange} style={styles.datePicker} />)}
+           
+           {showPicker && Platform.OS === 'ios' && (
+           <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+            <TouchableOpacity style={[
+                styles.button, styles.pickerButton, {backgroundColor: '#11182711'}
+            ]} onPress={toggleDatepicker}>
+                <Text style={[styles.buttonText, {color: '#075985'}]}>Cancel</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={[
+                styles.button, styles.pickerButton,
+            ]} onPress={confirmIOSDate}>
+                <Text style={[styles.buttonText]}>Confirm</Text>
+            </TouchableOpacity>
+           </View>
+           )}
+
            {!showPicker && (<Pressable onPress={toggleDatepicker}>
-             <TextInput style={styles.input} editable={false} placeholder='Date' value={formData.date} onChangeText={e => setFormData({...formData, date: e})}/>
+              
            </Pressable>)}
            <TouchableOpacity style={styles.button} onPress={() => onSubmit}>
             <Text>Submit</Text>
@@ -61,5 +83,17 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#DDDDDD',
         padding: 10,
+      },
+    buttonText: {
+        fontSize: 14,
+        fontWeight: '500',
+        color: '#fff'
+    },
+    datePicker: {
+        height: 120,
+        marginTop: -10
+      },
+    pickerButton: {
+        paddingHorizontal: 20
       }
 })
